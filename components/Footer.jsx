5fd61244120a9ca763896fc82523df9a85ad6197
Gauravsaini25 <saini.25gaurav@gmail.com>
2025-08-28 09:01:23 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    
+        <div className=' text-white font-bold text-xl h-[10vh] w-[100%] bg-[#1d293b] flex flex-col justify-center items-center'>
+            <div className="logo mx-8">
+                <span className='text-green-600'>&lt;</span><span>Pass</span><span className='text-green-600'>OP/&#62;</span>
+            </div>
+            <div className='text-lg'>
+                Created With ❤️ by Gaurav Saini
+            </div>
+
+            
+      
+    </div>
+  )
+}
+
+export default Footer
